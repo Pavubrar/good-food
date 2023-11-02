ae8463040b18d133ca30cfdfb38e50f27e19214e
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2023-11-02 11:39:05 -0600

    getting single recipe details

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchRecipes, fetchSingleRecipe } from "../utils/FetchData";
+import Loading from "../components/Loading";
 
 
-function RecipeDetail() {
-  return (
-    <div>RecipeDetail</div>
-  )
+const RecipeDetail = () => {
+const [recipe, setRecipe] = useState(null)
+const [recipeS, setRecipeS] = useState(null)
+const [loading, setLoading] =useState(false)
+
+const {id} = useParams()
+
+const getRecipe = async (id) =>{
+  try{
+    setLoading(true)
+  const data = await fetchSingleRecipe(id)
+  setRecipe(data)
+ 
+
+  const recommend = await fetchRecipes({query: recipe?.label, limit: 5})
+  setRecipeS(recommend)
+  setLoading(false)
+  } catch(error){
+    console.error();
+    setLoading(false)
+  }
 }
-
-export default RecipeDetail
+useEffect(()=>{
+  getRecipe(id)
+},[id])
+if(loading){
+  return (  
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <Loading/>
+    </div>
+  );
+}
+return(
+  
+)
+  
+}
+ 
+export default RecipeDetail;
