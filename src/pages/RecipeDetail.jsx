6192a8204fefe2f9f58177183fe03831c8a61c01
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2023-11-02 13:59:15 -0600

    design recipe card with detail

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchRecipes, fetchSingleRecipe } from "../utils/FetchData";
+import { AiFillPushpin } from "react-icons/ai"
+import { BsPatchCheck } from "react-icons/bs"
 import Loading from "../components/Loading";
+import Header from "../components/Header";
+import RecipeCard from "../components/RecipeCard";
 
 
 const RecipeDetail = () => {
@@ -28,6 +32,7 @@ const getRecipe = async (id) =>{
 }
 useEffect(()=>{
   getRecipe(id)
+// eslint-disable-next-line react-hooks/exhaustive-deps
 },[id])
 if(loading){
   return (  
@@ -37,7 +42,87 @@ if(loading){
   );
 }
 return(
-  
+  <div className='w-full'>
+    <Header
+      title={recipe?.label}
+      image={recipe?.image}
+    />
+    <div className="w-full px-4 lg:px-20 pt-5">
+      <div className="flex gap-10 items-center justify-center px-4">
+        <div className='flex flex-col justify-between'>
+          <span className='text-white text-center border border-gray-500 py-1.5 px-2 rounded-full mb-2'>{recipe.calories.toFixed(2)}</span>
+          <p className="text-neutral-100 text-[12px]" >CALORIES</p>
+        </div>
+        <div className='flex flex-col justify-center'>
+            <span className='text-white text-center border border-gray-500 py-1.5 rounded-full mb-2'>
+              {recipe?.totalTime}
+            </span>
+            <p className='text-neutral-100 text-[12px] md:text-md'>
+              TOTAL TIME
+            </p>
+          </div>
+
+          <div className='flex flex-col justify-center'>
+            <span className='text-white text-center border border-gray-500 py-1.5 rounded-full mb-2'>
+              {recipe?.yield}
+            </span>
+            <p className='text-neutral-100 text-[12px] md:text-md'>SERVINGS</p>
+          </div>
+      </div>
+      <div className='w-full flex flex-col md:flex-row gap-8 py-20 pxx-4 md:px-10'>
+          {/* LEFT SIDE */}
+          <div className='w-full md:w-2/4 md:border-r border-slate-800 pr-1'>
+            <div className='flex flex-col gap-5'>
+              <p className='text-green-500 text-2xl underline'>Ingredients</p>
+
+              {
+                recipe?.ingredientLines?.map((ingredient, index) => {
+                  return (
+                    <p key={index} className='text-neutral-100 flex gap-2'>
+                      <AiFillPushpin className='text-green-800 text-xl' /> {ingredient}
+                    </p>
+                  )
+                })
+              }
+            </div>
+
+            <div className='flex flex-col gap-3 mt-20'>
+              <p className='text-green-700 text-2xl underline'>Health Labels</p>
+
+              <div className='flex flex-wrap gap-4'>
+                {
+                  recipe?.healthLabels.map((item, index) => (
+                    <p className='text-white flex gap-2 bg-[#fff5f518] px-4 py-1 rounded-full ' key={index}>
+                      <BsPatchCheck color='green' /> {item}
+                    </p>
+                  ))
+                }
+
+              </div>
+            </div>
+
+          </div>
+          {/* RIGHT SIDE */}
+          <div className='w-full md:w-2/4 2xl:pl-10 mt-20 md:mt-0'>
+            {
+              recipeS?.length > 0 && (
+                <>
+                  <p className='text-white text-2xl'>Also Try This</p>
+
+                  <div className='flex flex-wrap gap-6 px-1 pt-3'>
+                    {
+                      recipeS?.map((item, index) => (
+                        <RecipeCard recipe={item} key={index} />
+                      ))
+                    }
+                  </div>
+                </>
+              )
+            }
+          </div>
+    </div>
+  </div>
+  </div>
 )
   
 }
